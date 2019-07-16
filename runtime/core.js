95ee972b8a71a11b8a311e98676bc9eaa9ac95a7
Kaden Thomas <thomas.kaden4@outlook.com>
2019-07-15 21:00:21 -0700

    Enforce code style

--- runtime/core.js
+++ runtime/core.js
@@ -1,17 +1,19 @@
 import fs from "fs";
 
 class Symbol {
-  constructor(symbol){
+  constructor(symbol) {
     this.symbol = symbol;
   }
 
-  apply(_, args){
+  apply(_, args) {
     console.log(`Apply for symbol "${this.symbol}" with`, args);
     return this;
   }
 }
 
-const errFunction = () => { throw new Error("todo"); };
+const errFunction = () => {
+  throw new Error("todo");
+};
 
 export const __process_pure = x => f => f(x);
 
@@ -22,12 +24,15 @@ export const __mpm_root = process.env["MPM_ROOT"];
 export const __stdout = errFunction;
 export const __stderr = errFunction;
 export const __stdin = () => {
-  let buff = Buffer.from(['']);
+  let buff = Buffer.from([""]);
   fs.readSync(0, buff, 0, 1, 0);
   return __process_pure(buff[0]);
 };
 
-export const __debug = o => { console.log(o); return o; };
+export const __debug = o => {
+  console.log(o);
+  return o;
+};
 export const __symbol = errFunction;
 
 export const __file_from_path = errFunction;
